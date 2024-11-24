a0047f6fc1870a8e00f1f91058755512bed95b8d
Dest <michael.octavo@ontariotechu.net>
2024-11-23 23:57:37 -0500

    Reconfigured venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Destly\Desktop\University Materials\Year4-3\Adv Web Dev\FinalProject\venv
+set VIRTUAL_ENV=C:\Users\Destly\Desktop\University Materials\Year4-3\Adv Web Dev\FinalProject-Git\Periscope\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
