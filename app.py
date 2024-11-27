899e4950e383b525b1b81a3512b905ca01e9a28b
Dest <michael.octavo@ontariotechu.net>
2024-11-27 18:58:54 -0500

    Finishing frontend-backend connections

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 # Dependencies
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import components.riotAPI
+from authlib.integrations.flask_client import OAuth
 
 # Setup
 app = Flask(__name__)
 app.static_folder = 'static'
+app.secret_key = 'PERI-KEYSCOPE10'
+
+# Github OAuth config
+oauth = OAuth(app)
+github = oauth.register(
+    name='github',
+    client_id='Ov23li1wFmJXgVWxcixw',
+    client_secret='5595aa46aa861bc66f209b3e8fc484d84c43c3b8',
+    access_token_url='https://github.com/login/oauth/access_token',
+    access_token_params=None,
+    authorize_url='https://github.com/login/oauth/authorize',
+    authorize_params=None,
+    api_base_url='https://api.github.com/',
+    client_kwargs={'scope': 'user:email'},
+)
 
 # Setting up the main page
 @app.route('/', methods=('GET', 'POST'))
 def index():
+    user = session.get('user')
+    if user:
+        print(user)
 
     # If there is content to be added, this code is executed instead
     if request.method == "POST":
@@ -25,6 +44,9 @@ def index():
         MatchInfo = components.riotAPI.printMatch(name)
         RankInfo = components.riotAPI.printPlayerRank(name)
 
+        # WIP
+        #components.riotAPI.printMatchStatistics(name)
+
         # Add to main info (naive implementation)
         #SummonerInfo.append(SummonerName)
         #SummonerInfo.append(SummonerLevel)
@@ -37,10 +59,29 @@ def index():
 
 # Login 
 # Landing page
+@app.route('/login_landing')
+def login_landing():
+    return render_template('login.html')
+
 @app.route('/login')
 def login():
+    # Replace with a landing page later
+    #return render_template('login.html')
+    return github.authorize_redirect(url_for('authorize', _external=True))
 
-    return render_template('login.html')
+@app.route('/authorize')
+def authorize():
+     # Fetch the access token
+    token = github.authorize_access_token()
+    # Fetch user information from GitHub
+    user = github.get('user').json()
+    session['user'] = user  # Store user info in session
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 
 # Initiate login
 @app.route('/login_success')
--- components/riotAPI.py
+++ components/riotAPI.py
@@ -23,6 +23,7 @@ def printSummoner():
 # Primitive
 # 1 API Call
 def printSummonerV2(nameInputNonSplit):
+    print("Print Summoner Called".center(100, "-"))
     # Separate the player name and tagline
     nameInput, taglineInput = nameInputNonSplit.split("#")
 
@@ -68,14 +69,15 @@ def printSummonerV2(nameInputNonSplit):
 # [1][n] = Blue Champions
 # [2][n] = Red Players
 # [3][n] = Red Champions
-# [4] = Blue / Red Victory
+# [4][n] = Player Statistics
+# [5] = Blue / Red Victory
 
 # Match History (variable match length)
 # 4 API Calls / match
 numMatches = 2
 
 def printMatch(nameInputNonSplit):
-
+    print("Print Match Called".center(100, "-"))
     # Separate the player name and tagline
     nameInput, taglineInput = nameInputNonSplit.split("#")
 
@@ -84,30 +86,81 @@ def printMatch(nameInputNonSplit):
 
     match_history = summoner.match_history
     match = match_history[0]
-    print("Match ID:", match.id)
+    #pprint(vars(match))
+    #print("Match ID:", match.id)
 
     print("Blue team won?", match.blue_team.win)
     print("Red team won?", match.red_team.win)
-    print()
 
-    print("Participants on blue team:")
+    # All available data points, for the purpose of our app, we will only need a few of these
+    '''
+    "summoner": p.summoner_name,
+    "champion": p.champion.name,
+    "runes": p.runes.keystone.name,
+    "d_spell": p.summoner_spell_d.name,
+    "f_spell": p.summoner_spell_f.name,
+    "kills": p.stats.kills,
+    "assist": p.stats.assists,
+    "deaths": p.stats.deaths,
+    "kda_ratio": round(p.stats.kda, 2),
+    "damage_dealt": p.stats.total_damage_dealt,
+    "creep_score": p.stats.total_minions_killed,
+    "vision_score": p.stats.vision_score,
+    '''
+
+    #print("Participants on blue team:")
+    # Blue team players and champion fetch
     BlueTeamPlayers = [p.summoner_name for p in match.blue_team.participants]
     BlueTeamChampions = [p.champion.name for p in match.blue_team.participants]
-    for p in match.blue_team.participants:
-        print(f"{p.summoner_name}: {p.champion.name}")
-    print()
+    print(BlueTeamPlayers)
 
-    print("Participants on red team:")
+    #print("Participants on red team:")
+    # Red team players and champion fetch
     RedTeamPlayers = [p.summoner_name for p in match.red_team.participants]
     RedTeamChampions = [p.champion.name for p in match.red_team.participants]
-    
-    #for p in match.red_team.participants:
-    #    print(f"{p.summoner_name}: {p.champion.name}")
-    #print()
+    print(RedTeamPlayers)
 
-    print("Keystone and stat runes for each player:")
-    for p in match.participants:
-        print(f"{p.champion.name}: {p.runes.keystone.name}, ({', '.join([r.name for r in p.stat_runes])})")
+    PlayerStats = []
+    PlayerGameOutcome = ''
+    # Search for our specific summoner and log its statistics
+    if (nameInput in BlueTeamPlayers):
+        print("Destly was here on Blue")
+        # log statistics
+        for p in match.blue_team.participants:
+            #print(p.summoner_name)
+            if p.summoner_name == nameInput:
+                PlayerStats.append(p.summoner_name)
+                PlayerStats.append(p.champion.name)
+                PlayerStats.append(p.runes.keystone.name)
+                PlayerStats.append(p.stats.kills)
+                PlayerStats.append(p.stats.deaths)
+                PlayerStats.append(p.stats.assists)
+                PlayerStats.append(round(p.stats.kda, 2))
+
+                if (match.blue_team.win == True):
+                    PlayerGameOutcome = 'Victory'
+                else:
+                    PlayerGameOutcome = 'Defeat'
+            
+    if (nameInput in RedTeamPlayers):
+        # log statistics
+        for p in match.blue_team.participants:
+            #print(p.summoner_name)
+            if p.summoner_name == nameInput:
+                PlayerStats.append(p.summoner_name)
+                PlayerStats.append(p.champion.name)
+                PlayerStats.append(p.runes.keystone.name)
+                PlayerStats.append(p.stats.kills)
+                PlayerStats.append(p.stats.deaths)
+                PlayerStats.append(p.stats.assists)
+                PlayerStats.append(round(p.stats.kda, 2))
+
+                if (match.red_team.win == True):
+                    PlayerGameOutcome = 'Victory'
+                else:
+                    PlayerGameOutcome = 'Defeat'
+
+    print(PlayerStats)
 
     # Appending all information
     MatchInfo = []
@@ -115,17 +168,15 @@ def printMatch(nameInputNonSplit):
     MatchInfo.append(BlueTeamChampions)
     MatchInfo.append(RedTeamPlayers)
     MatchInfo.append(RedTeamChampions)
-    print(BlueTeamPlayers)
+    MatchInfo.append(PlayerStats)
+    MatchInfo.append(PlayerGameOutcome)
+    #print(BlueTeamPlayers)
     
 
     return MatchInfo
 
-
-# Selective API Calls
-def printNewestMatch():
-    ...
-
 def printPlayerRank(nameInputNonSplit):
+    print("Print Rank Called".center(100, "-"))
     # Separate the player name and tagline
     nameInput, taglineInput = nameInputNonSplit.split("#")
 
@@ -141,3 +192,4 @@ def printPlayerRank(nameInputNonSplit):
         RankInfo.append(str(entry.division))
     #print(PlayerRanks)
     return RankInfo
+
