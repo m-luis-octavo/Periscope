a52323005d48ff4c4384cc24bf3d32981bc67010
Dest <michael.octavo@ontariotechu.net>
2024-11-28 02:43:17 -0500

    API Key Change + Requirements

--- components/riotAPI.py
+++ components/riotAPI.py
@@ -1,7 +1,7 @@
 # Python wrapper of the Riot API
 import cassiopeia as cass
 # API Key, resets daily
-cass.set_riot_api_key("RGAPI-e0d91437-cc22-4611-a028-5d8962eaf8f6")
+cass.set_riot_api_key("RGAPI-6f9f5620-e395-4646-8ea1-dc18065c0c99")
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------------- #
 
